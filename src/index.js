9b5dee665f96f3a4289b1c2418a5a96019c33a58
sayuri0709 <53500693+sayuri0709@users.noreply.github.com>
2022-05-25 15:16:52 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+/***
+ *  テンプレート文字列
+ *
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const name = "じゃけえ";
+const age = 28;
+
+const message = `私の名前は${name}です。年齢は${age}です。`;
+
+console.log(message);
