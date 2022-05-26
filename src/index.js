1dc23d1ccb5046994d1811c476a06e65ac30b0ea
sayuri0709 <53500693+sayuri0709@users.noreply.github.com>
2022-05-26 17:11:36 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
  *
  */
 
-const name = "じゃけえ";
-const age = 28;
+// const name = "じゃけえ";
+// const age = 28;
 
-const message = `私の名前は${name}です。年齢は${age}です。`;
+// const message = `私の名前は${name}です。年齢は${age}です。`;
 
-console.log(message);
+// console.log(message);
 
 /***
  *  アロー関数
@@ -27,3 +27,27 @@ const func3 = (num1, num2) => {
   return num1 + num2;
 };
 console.log(func3(10, 20));
+
+/***
+ *  分割代入
+ *
+ */
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ["じゃけえ", 28];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const messge4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(messge4);
